81f14b85b8156e67d668bb70e8d9448df7dfbeac
Contra <contra@maricopa.edu>
2016-01-29 20:43:12 -0800

    2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## Change Log
 
-### upcoming (2016/01/29 00:43 +00:00)
+### upcoming (2016/01/30 04:40 +00:00)
+- [7f77f19](https://github.com/contra/thinky-export-schema/commit/7f77f19cb7a9b14a29685de4cbfa0e792f05670a) 1.0.1 (@contra)
+- [927d178](https://github.com/contra/thinky-export-schema/commit/927d17843e5b9bf74b545266eda166112c07745d) pkg fix (@contra)
 - [1099895](https://github.com/contra/thinky-export-schema/commit/1099895324bf4a70bf06ee57d93e4406335e525d) keywords (@contra)
 - [9c2f4e2](https://github.com/contra/thinky-export-schema/commit/9c2f4e2d94cc3174e04421484a80b139535e2d5e) 1.0.0 (@contra)
--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,43 @@ var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var exportSchema = function exportSchema(schema) {
+var mapName = function mapName(v) {
+  return v.constructor.name.replace(/^Type/, '');
+};
+
+var getFields = function getFields(schema) {
   return (0, _lodash2.default)(schema, function (v) {
     if (v._schema) {
-      return exportSchema(v._schema);
+      if (v.constructor && v.constructor.name === 'TypeArray') {
+        return [mapName(v._schema)];
+      }
+      return getFields(v._schema);
     } else {
-      return v.constructor.name.replace(/^Type/, '');
+      return mapName(v);
     }
   });
 };
 
+var getJoins = function getJoins(model) {
+  return (0, _lodash2.default)(model._joins, function (v) {
+    return {
+      type: v.type,
+      leftKey: v.leftKey,
+      rightKey: v.rightKey
+    };
+  });
+};
+
+var exportSchema = function exportSchema(model) {
+  return {
+    fields: getFields(model._schema._schema),
+    relationships: getJoins(model),
+    validation: {} // TODO
+  };
+};
+
 exports.default = function (model) {
-  return exportSchema(model._schema._schema);
+  return exportSchema(model);
 };
 
 module.exports = exports['default'];
